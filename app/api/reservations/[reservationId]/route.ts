54adc5098d0993a1f86960bb074cc634f6a86f78
Oskar Gawlak <oskar.gawlak@gmail.com>
2023-05-27 01:43:35 +0200

    feat: add api endpoint for deleting a reservation

---
+++ app/api/reservations/[reservationId]/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+interface Params {
+  reservationId: string;
+}
+
+export async function DELETE(request: Request, { params }: { params: Params }) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { reservationId } = params;
+
+  if (!reservationId || typeof reservationId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  const reservation = await prisma?.reservation.deleteMany({
+    where: {
+      id: reservationId,
+      OR: [{ userId: currentUser.id }],
+    },
+  });
+
+  return NextResponse.json(reservation);
+}
